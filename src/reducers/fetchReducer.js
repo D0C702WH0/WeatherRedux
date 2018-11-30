14e4c059bc6e8bea2834f257cfd682e72db4ea37
D0C702WH0 <jemyplu@gmail.com>
2018-11-30 18:13:10 +0100

    display name temp weather

--- src/reducers/fetchReducer.js
+++ src/reducers/fetchReducer.js
@@ -1,19 +1,21 @@
-import { FETCH_DATA } from '../actions/types';
+import { FETCH_DATA } from "../actions/types";
 
 const initialState = {
   weatherInfo: [],
+  isLoaded: false
   //Write here your state properties for this reducer with there initial value
 };
 
-export default function (state = initialState, action) {
+export default function(state = initialState, action) {
   switch (action.type) {
     case FETCH_DATA:
       return {
         ...state,
         weatherInfo: action.weatherInfo,
+        isLoaded: action.isLoaded
       };
-    default : {
-        return state
+    default: {
+      return state;
     }
   }
 }
